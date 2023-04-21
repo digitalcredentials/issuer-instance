dc89abcb2a23fe889b1c08588d5c695d66e2fd60
James Chartrand <jc.chartrand@gmail.com>
2023-04-21 15:43:44 -0400

    add libs

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,4 @@
 declare module '@digitalcredentials/vc'
+declare module '@digitalcredentials/did-method-key'
+declare module '@digitalcredentials/bnid'
+declare module '@digitalcredentials/ed25519-signature-2020'
